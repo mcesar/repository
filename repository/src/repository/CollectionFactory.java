e4cc1ab42d16d2ac277ca860471727d4bd33afc5
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2013-10-31 16:00:16 -0200

    Implementação da classe CollectionFactory, que cria coleções encapsuladoras de VoltTables

---
+++ repository/src/repository/CollectionFactory.java
@@ -0,0 +1,58 @@
+package repository;
+
+import java.util.*;
+import org.voltdb.*;
+
+public class CollectionFactory {
+
+	public static class VoltCollection extends AbstractCollection<Object[]> {
+
+		private VoltTable table;
+
+		private VoltIterator iterator;
+
+		public VoltCollection(VoltTable table) {
+			this.table = table;
+			this.iterator = new VoltIterator(table);
+		}
+
+		public Iterator<Object[]> iterator() {
+			return iterator;
+		}
+
+		public int size() {
+			return table.getRowCount();
+		}
+	}
+
+	public static class VoltIterator implements Iterator<Object[]> {
+
+		private VoltTable table;
+
+		public VoltIterator(VoltTable table) {
+			this.table = table;
+		}
+
+		public void remove() {
+		}
+
+		public Object[] next() {
+			table.advanceRow();
+			Object[] result = new Object[table.getColumnCount()];
+			for (int i = 0; i < table.getColumnCount(); i++) {
+				result[i] = table.get(i, table.getColumnType(i));
+			}
+			return result;
+		}
+
+		public boolean hasNext() {
+			return table.getActiveRowIndex() < table.getRowCount() - 1;
+		}
+	}
+
+	public static Collection<Object[]> newVoltCollection(VoltTable[] tables) {
+		if (tables == null || tables.length == 0) throw new IllegalArgumentException();
+		return new VoltCollection(tables[0]);
+	}
+
+}
---
+++ repository/tests/repository/CollectionFactoryTest.java
@@ -0,0 +1,84 @@
+package repository;
+
+import java.util.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.voltdb.*;
+
+public class CollectionFactoryTest {
+
+	private Collection<Object[]> col1;
+
+	private Collection<Object[]> col2;
+
+	@Before
+	public void setup() {
+		VoltTable[] t1 = { new VoltTable(new VoltTable.ColumnInfo("col1", VoltType.BIGINT)) };
+		t1[0].addRow(1);
+		col1 = CollectionFactory.newVoltCollection(t1);
+
+		VoltTable[] t2 = { new VoltTable(new VoltTable.ColumnInfo("col1", VoltType.BIGINT), 
+			new VoltTable.ColumnInfo("col2", VoltType.STRING)) };
+		t2[0].addRow(1, "a");
+		t2[0].addRow(2, "b");
+		col2 = CollectionFactory.newVoltCollection(t2);
+	}
+
+	@Test
+	public void newVoltCollection_NullTable() {
+		try {
+			Collection<Object[]> col = CollectionFactory.newVoltCollection(null);
+			fail("Não é permitido o uso de coleções nulas");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	public void newVoltCollection_TableWithOneRow_size() {
+		assertEquals(1, col1.size());
+	}
+
+	@Test
+	public void newVoltCollection_TableWithOneRow_hasNext() {
+		Iterator<Object[]> it = col1.iterator();
+		assertTrue(it.hasNext());
+		it.next();
+		assertFalse(it.hasNext());
+	}
+
+	@Test
+	public void newVoltCollection_TableWithOneRow_next() {
+		Iterator<Object[]> it = col1.iterator();
+		Object[] arr = it.next();
+		assertEquals(1l, arr[0]);
+	}
+
+	@Test
+	public void newVoltCollection_TableWithTwoRows_size() {
+		assertEquals(2, col2.size());
+	}
+
+	@Test
+	public void newVoltCollection_TableWithTwoRows_hasNext() {
+		Iterator<Object[]> it = col2.iterator();
+		assertTrue(it.hasNext());
+		it.next();
+		assertTrue(it.hasNext());
+		it.next();
+		assertFalse(it.hasNext());
+	}
+
+	@Test
+	public void newVoltCollection_TableWithTwoRows_next() {
+		Iterator<Object[]> it = col2.iterator();
+		Object[] arr = it.next();
+		assertEquals(1l, arr[0]);
+		assertEquals("a", arr[1]);
+		arr = it.next();
+		assertEquals(2l, arr[0]);
+		assertEquals("b", arr[1]);
+	}
+
+}
